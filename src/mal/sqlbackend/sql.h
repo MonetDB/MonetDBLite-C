37dfafa5ab87d678933e9b9fc2f8ae45f419fa72
Pedro Ferreira <pedrotadim@outlook.com>
2018-03-25 16:32:14 +0200

    Missing function prototype

--- src/mal/sqlbackend/sql.h
+++ src/mal/sqlbackend/sql.h
@@ -59,6 +59,7 @@ sql5_export str SQLabort(Client cntxt, MalBlkPtr mb, MalStkPtr stk, InstrPtr pci
 sql5_export str SQLtransaction2(Client cntxt, MalBlkPtr mb, MalStkPtr stk, InstrPtr pci);
 sql5_export str SQLcatalog(Client cntxt, MalBlkPtr mb, MalStkPtr stk, InstrPtr pci);
 
+sql5_export str mvc_grow_wrap(Client cntxt, MalBlkPtr mb, MalStkPtr stk, InstrPtr pci);
 sql5_export str mvc_append_wrap(Client cntxt, MalBlkPtr mb, MalStkPtr stk, InstrPtr pci);
 sql5_export str mvc_append_column(sql_trans *t, sql_column *c, BAT *ins);
 
