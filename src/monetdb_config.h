43f8c29578887abcd411463ecef5f87e78402a66
Hannes Muehleisen <hannes@muehleisen.org>
2018-07-09 17:35:19 +0200

    minor leftovers

--- src/monetdb_config.h
+++ src/monetdb_config.h
@@ -5,7 +5,9 @@
 #ifndef _SEEN_MONETDB_CONFIG_H
 #define _SEEN_MONETDB_CONFIG_H 1
 
-
+#ifdef _MSC_VER
+#define NATIVE_WIN32
+#endif
 
 
 #ifdef __MINGW32__
@@ -901,13 +903,7 @@ typedef lng ptrdiff_t;
 #undef HAVE_SYS_PARAM_H
 #undef HAVE_SYS_TIME_H
 #undef HAVE_UNISTD_H
-
-
-
-
-
-
-
+#undef HAVE_DIRENT_H
 
 #endif
 
