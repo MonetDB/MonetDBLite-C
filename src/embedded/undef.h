cb8f279676c295ba85670ee57c808429499c1931
Pedro Ferreira <pedrotadim@outlook.com>
2018-10-03 14:49:15 +0200

    Add undefs back.

--- src/embedded/undef.h
+++ src/embedded/undef.h
@@ -2,10 +2,10 @@ extern FILE* embedded_stdout;
 extern FILE* embedded_stderr;
 
 #define exit(status) ((void) (status))
-//#undef stdout
-//#define stdout embedded_stdout
-//#undef stderr
-//#define stderr embedded_stderr
+#undef stdout
+#define stdout embedded_stdout
+#undef stderr
+#define stderr embedded_stderr
 
 #ifdef HAVE_EMBEDDED_R
 #define srand(seed) ((void) (seed))
