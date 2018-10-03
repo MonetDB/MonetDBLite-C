d8b1b5e729678923a7f1a52eff9c775519eb8b5e
Pedro Ferreira <pedrotadim@outlook.com>
2018-10-03 15:52:33 +0200

    Reverting change.

--- src/sql/storage/bat/nop_logger.h
+++ src/sql/storage/bat/nop_logger.h
@@ -11,21 +11,6 @@
 
 #include "sql_storage.h"
 
-/* this function is (currently) only used in msabaoth and sql;
- * msabaoth is part of monetdb5 and we want this function to be
- * exported so that the call in sql can be satisfied by the version
- * that is included in monetdb5 */
-
-#ifdef WIN32
-#if !defined(LIBMSABAOTH) && !defined(LIBMUUID)
-#define loggerexport extern __declspec(dllimport)
-#else
-#define loggerexport extern __declspec(dllexport)
-#endif
-#else
-#define loggerexport extern
-#endif
-
-loggerexport void nop_logger_init( logger_functions *lf );
+extern void nop_logger_init( logger_functions *lf );
 
 #endif /*NOP_LOGGER_H */
